f9be923261a63e5a875a3e069718a4486e732f26
Suyash Sapkal <suyashsapkal21@gmail.com>
2021-02-05 00:07:27 +0530

    complete changes done
    Changes done in HTML files in form folder. Changes done in DB models.

--- starter_code/app.py
+++ starter_code/app.py
@@ -53,12 +53,17 @@ class Venue(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+    genres = db.Column(db.ARRAY(db.String()))
+    address = db.Column(db.String(120))
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
     phone = db.Column(db.String(120))
+    website = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String(120))
+    
     shows = db.relationship('Artist', secondary=Shows,backref='Venue',lazy=True)
 
     
@@ -70,12 +75,17 @@ class Artist(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+    genres = db.Column(db.ARRAY(db.String()))
+    address = db.Column(db.String(120))
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
+    website = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String(120))
+
     shows = db.relationship('Venue', secondary=Shows,backref='Artist',lazy=True)
     
 
